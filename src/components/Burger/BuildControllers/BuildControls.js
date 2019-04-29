85ceaedce50e4a99870935c43a569a5ce20aec6a
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-29 17:36:08 +0200

    Disabled Button

--- src/components/Burger/BuildControllers/BuildControls.js
+++ src/components/Burger/BuildControllers/BuildControls.js
@@ -18,6 +18,7 @@ export default function BuildControls(props) {
           key={ctrl.label}
           label={ctrl.label}
           added={() => props.ingredientAdded(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
           removed={() => props.ingredientRemoved(ctrl.type)}
         />
       ))}
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -42,17 +42,29 @@ export default class BurgerBuilder extends Component {
         totalPrice: prevState.totalPrice - INGREDIENT_PRICES[type],
         ingredients: {
           ...prevState.ingredients,
-          [type]: (prevState.ingredients[type]) ? prevState.ingredients[type] - 1 : 0
+          [type]: prevState.ingredients[type] ? prevState.ingredients[type] - 1 : 0
         }
       };
     });
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+        />
       </Aux>
     );
   }
