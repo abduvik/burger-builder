a204e16bc17def44e8845a6ab0247c796aef60ce
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-05-23 00:59:23 +0200

    Create Orders page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
     <div>
       <Layout>
         <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
     </div>
