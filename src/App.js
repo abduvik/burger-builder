af60be827921edd2b6b58e6b54e99031200c1e43
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-06-21 23:59:30 +0200

    Lazy loading of components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import { connect } from "react-redux";
 import { authCheckState } from "./store/actions/auth.actions";
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 class App extends React.Component {
   componentDidMount() {
@@ -15,12 +14,15 @@ class App extends React.Component {
   }
   
   render() {
+    const AsyncCheckout = asyncComponent(() => import('./containers/Checkout/Checkout'));
+    const AsyncOrders = asyncComponent(() => import('./containers/Orders/Orders'));
+
     return (
       <div>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
+            <Route path="/checkout" component={AsyncCheckout} />
+            <Route path="/orders" component={AsyncOrders} />
             <Route path="/auth" component={Auth} />
             <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
